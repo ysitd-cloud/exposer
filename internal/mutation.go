a04f123502289f80ec008b1187b2c79d59585eb1
Tony Yip <tony@opensource.hk>
2018-04-25 14:34:59 +0800

    Add mutation type

---
+++ internal/mutation.go
@@ -0,0 +1,16 @@
+package internal
+
+type action string
+
+const (
+	Create action = "create"
+	Update        = "update"
+	Delete        = "delete"
+)
+
+type Mutation struct {
+	Action      action
+	Hostname    string
+	ServiceName string
+	Port        int
+}
